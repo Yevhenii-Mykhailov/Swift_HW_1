501538b872256092bb2da70edc2d4c1ad2584fdf
Yevhenii Mykhailovskyi <mihaylovskiyeugeny@gmail.com>
2022-09-28 14:37:04 +0300

    Initial Commit

---
+++ Homework 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 1
+//
+//  Created by Yevhenii M on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
